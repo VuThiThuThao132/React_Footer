035ed306308a8126e7b43d111e03b7cf48832442
Thu Thao <vuthithuthao132@gmail.com>
2022-07-19 11:35:37 +0700

    React_Footer

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 80px 60px;
+    background: radial-gradient(circle, #ffeaa7 0%, #fdcb6e 100%)
+`
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+`
+export const Column = styled.div`
+    display:  flex;
+    flex-direction: column;
+    text-align: left;
+    margin-left: 60px;
+`
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px,1fr));
+    grid-gap: 20px;
+
+    @media (max-width: 1000px) {
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
+`
+export const Link = styled.a`
+    color: black;
+    margin-bottom: 20px;
+    font-size: 18px;
+    text-decoration: none;
+
+    &:hover {
+        color: #fff;
+        transition: 200ms ease-in;
+    }
+
+`
+export const Title = styled.p`
+    font-size : 20px;
+    color: black;
+    margin-bottom: 40px;
+    font-weight: bold;
+
+`
+
+
